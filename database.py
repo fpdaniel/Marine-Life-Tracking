11dc873e11a8d9a46ac6ad66084ba94e8d95d222
fpdaniel <fpdaniel2@gmail.com>
2018-04-27 10:03:47 -0700

    Create more tables and make small changes to class methods.

--- database.py
+++ database.py
@@ -1,25 +1,42 @@
 import sqlite3
 
+
 class MarineDatabase:
-    """ Local database class that holds data """
+    """Local database class that holds data."""
+
     locale = "testing.db"
 
     def __init__(self):
-        """ Initialize class variables and creates connection to db """
+        """Initialize class variables and creates connection to db """
         self.conn = sqlite3.connect(MarineDatabase.locale)
         self.curr = self.conn.cursor()
 
-    def create_table_locations(self):
-        """ Create table with the name of the table being locations and one column for beach """
-        self.curr.execute(''' CREATE TABLE IF NOT EXISTS locations
-                              (beach)''')
+    def create_tables(self):
+        """Create the tables if they don't exist."""
+        self.curr.execute('''CREATE TABLE IF NOT EXISTS locations (
+                            id INTEGER PRIMARY KEY,
+                            name TEXT NOT NULL
+                            )''')
+        self.curr.execute('''CREATE TABLE IF NOT EXISTS animals (
+                            id INTEGER PRIMARY KEY,
+                            name TEXT NOT NULL
+                            )''')
+        self.curr.execute('''CREATE TABLE IF NOT EXISTS observations (
+                            id INTEGER PRIMARY KEY,
+                            date TEXT NOT NULL,
+                            temperature INTEGER NOT NULL,
+                            location_id INTEGER NOT NULL,
+                            animal_id INTEGER NOT NULL,
+                            amount INTEGER
+                            )''')
         self.conn.commit()
 
     def execute_location(self, location_name):
-        self.curr.execute(" INSERT INTO locations VALUES (?)", (location_name,))
+        self.curr.execute("INSERT INTO locations VALUES (?)", (location_name,))
 
     def commit(self):
         self.conn.commit()
 
     def close(self):
+        self.curr.close()
         self.conn.close()
